7fae7537c1a1d39945902ed48ea7bed37a9fadbc
borz7zy <borz7zy@yandex.ru>
2025-02-04 17:43:51 +0100

    added player event handlers

--- main.cpp
+++ main.cpp
@@ -15,26 +15,21 @@ extern "C"
 // This should use an abstract interface if it is to be passed to other components.  Like the files
 // in `<Server/Components/>` you would share only this base class and keep the implementation
 // private.
-class OmpLua final : public IComponent, public PoolEventHandler<IVehicle>
+class OmpLua final : public IComponent,
+                     public PlayerConnectEventHandler,
+                     public PlayerSpawnEventHandler,
+                     public PlayerStreamEventHandler,
+                     public PlayerTextEventHandler,
+                     public PlayerShotEventHandler,
+                     public PlayerChangeEventHandler,
+                     public PlayerDamageEventHandler,
+                     public PlayerClickEventHandler,
+                     public PlayerCheckEventHandler,
+                     public PlayerUpdateEventHandler
 {
 private:
     // Hold a reference to the main server core.
     ICore *core_ = nullptr;
-
-    // Hold a reference to the vehicle component so methods in it can be called.
-    IVehiclesComponent *vehicles_ = nullptr;
-
-    void updateVehicleCount()
-    {
-        for (INetwork *network : core_->getNetworks())
-        {
-            INetworkQueryExtension *query = queryExtension<INetworkQueryExtension>(network);
-            if (query)
-            {
-                query->addRule("vehicles", std::to_string(vehicles_->count()));
-            }
-        }
-    }
 
 public:
     // Visit https://open.mp/uid to generate a new unique ID.
@@ -43,23 +38,35 @@ public:
     // When this component is destroyed we need to tell any linked components this it is gone.
     ~OmpLua()
     {
-        // Clean up what you did above.
-        if (vehicles_)
-        {
-            vehicles_->getPoolEventDispatcher().removeEventHandler(this);
-        }
     }
 
-    // Implement the vehicle pool listener API.
-    void onPoolEntryCreated(IVehicle &entry) override
-    {
-        updateVehicleCount();
-    }
-
-    void onPoolEntryDestroyed(IVehicle &entry) override
-    {
-        updateVehicleCount();
-    }
+    void onIncomingConnection(IPlayer &player, StringView ipAddress, unsigned short port) override {}
+    void onPlayerConnect(IPlayer &player) override {}
+    void onPlayerDisconnect(IPlayer &player, PeerDisconnectReason reason) override {}
+    void onPlayerClientInit(IPlayer &player) override {}
+    bool onPlayerRequestSpawn(IPlayer &player) override { return true; }
+    void onPlayerSpawn(IPlayer &player) override {}
+    void onPlayerStreamIn(IPlayer &player, IPlayer &forPlayer) override {}
+    void onPlayerStreamOut(IPlayer &player, IPlayer &forPlayer) override {}
+    bool onPlayerText(IPlayer &player, StringView message) override { return true; }
+    bool onPlayerCommandText(IPlayer &player, StringView message) override { return false; }
+    bool onPlayerShotMissed(IPlayer &player, const PlayerBulletData &bulletData) override { return true; }
+    bool onPlayerShotPlayer(IPlayer &player, IPlayer &target, const PlayerBulletData &bulletData) override { return true; }
+    bool onPlayerShotVehicle(IPlayer &player, IVehicle &target, const PlayerBulletData &bulletData) override { return true; }
+    bool onPlayerShotObject(IPlayer &player, IObject &target, const PlayerBulletData &bulletData) override { return true; }
+    bool onPlayerShotPlayerObject(IPlayer &player, IPlayerObject &target, const PlayerBulletData &bulletData) override { return true; }
+    void onPlayerScoreChange(IPlayer &player, int score) override {}
+    void onPlayerNameChange(IPlayer &player, StringView oldName) override {}
+    void onPlayerInteriorChange(IPlayer &player, unsigned newInterior, unsigned oldInterior) override {}
+    void onPlayerStateChange(IPlayer &player, PlayerState newState, PlayerState oldState) override {}
+    void onPlayerKeyStateChange(IPlayer &player, uint32_t newKeys, uint32_t oldKeys) override {}
+    void onPlayerDeath(IPlayer &player, IPlayer *killer, int reason) override {}
+    void onPlayerTakeDamage(IPlayer &player, IPlayer *from, float amount, unsigned weapon, BodyPart part) override {}
+    void onPlayerGiveDamage(IPlayer &player, IPlayer &to, float amount, unsigned weapon, BodyPart part) override {}
+    void onPlayerClickMap(IPlayer &player, Vector3 pos) override {}
+    void onPlayerClickPlayer(IPlayer &player, IPlayer &clicked, PlayerClickSource source) override {}
+    void onClientCheckResponse(IPlayer &player, int actionType, int address, int results) override {}
+    bool onPlayerUpdate(IPlayer &player, TimePoint now) override { return true; }
 
     // Implement the main component API.
     StringView componentName() const override
@@ -82,26 +89,15 @@ public:
     void onInit(IComponentList *components) override
     {
         // Cache components, add event handlers here.
-        vehicles_ = components->queryComponent<IVehiclesComponent>();
-        if (vehicles_)
-        {
-            vehicles_->getPoolEventDispatcher().addEventHandler(this);
-        }
     }
 
     void onReady() override
     {
         // Fire events here at earliest.
-        updateVehicleCount();
     }
 
     void onFree(IComponent *component) override
     {
-        // Invalidate vehicles_ pointer so it can't be used past this point.
-        if (component == vehicles_)
-        {
-            vehicles_ = nullptr;
-        }
     }
 
     void free() override
